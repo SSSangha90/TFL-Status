ca91e97312f000834cda2a0dac54e120ecfe01e2
Sukhveer Sangha <sukhveers@push.live>
2023-08-29 13:39:34 +0100

    Filter stations

--- src/TFL/tfl.jsx
+++ src/TFL/tfl.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 const TFL = () => {
   const [data, setData] = useState([]);
+  const [filter, setFilter] = useState("");
 
   const fetchTubeData = () => {
     fetch("https://api.tfl.gov.uk/Line/Mode/tube/status")
@@ -10,6 +11,7 @@ const TFL = () => {
       })
       .then((d) => {
         const services = d.map((s) => ({
+          id: d.id,
           station: s.name,
           service: s.lineStatuses[0].statusSeverityDescription,
         }));
@@ -21,20 +23,36 @@ const TFL = () => {
     fetchTubeData();
   }, []);
 
+  const stationInformation = (d) => {
+    console.log(d);
+  };
+
+  const filterStations = useCallback((e) => {
+    setFilter(e.target.value);
+  }, []);
+
+  // groups.filter((group) => group.title.toLowerCase().includes(search.toLowerCase()))
+  const filteredData = useMemo(() => {
+    return data
+      .filter((d) => d.station.toLowerCase().includes(filter.toLowerCase()))
+      .map((d) => (
+        <tr key={d.id} onClick={stationInformation(d.id)}>
+          <td>{d.station}</td>
+          <td>{d.service}</td>
+        </tr>
+      ));
+  }, [data, filter]);
+
   return (
     <>
       <h2>TFL Updates</h2>
+      <input type="text" value={filter} onChange={filterStations} />
       <table>
         <tr>
           <th>Station</th>
           <th>Status</th>
         </tr>
-        {data.map((d) => (
-          <tr key={d.station}>
-            <td>{d.station}</td>
-            <td>{d.service}</td>
-          </tr>
-        ))}
+        {filteredData}
       </table>
     </>
   );
