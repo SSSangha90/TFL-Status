7d6326b3a046154b93516de7535820ab1bf7b02d
Sukhveer Sangha <sukhveers@push.live>
2023-08-29 13:22:59 +0100

    Fetch data, set state, map and return table data

--- src/TFL/tfl.jsx
+++ src/TFL/tfl.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TFL = () => {
-  return <h2>TFL Updates</h2>;
+  const [data, setData] = useState([]);
+
+  const fetchTubeData = () => {
+    fetch("https://api.tfl.gov.uk/Line/Mode/tube/status")
+      .then((response) => {
+        return response.json();
+      })
+      .then((d) => {
+        const services = d.map((s) => ({
+          station: s.name,
+          service: s.lineStatuses[0].statusSeverityDescription,
+        }));
+        setData(services);
+      });
+  };
+
+  useEffect(() => {
+    fetchTubeData();
+  }, []);
+
+  return (
+    <>
+      <h2>TFL Updates</h2>
+      <table>
+        <tr>
+          <th>Station</th>
+          <th>Status</th>
+        </tr>
+        {data.map((d) => (
+          <tr key={d.station}>
+            <td>{d.station}</td>
+            <td>{d.service}</td>
+          </tr>
+        ))}
+      </table>
+    </>
+  );
 };
 
 export default TFL;
